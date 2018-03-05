a54f3141755cd908d7311d0a6702919ed492079e
refuse1993 <refuse1993@gmail.com>
2018-03-05 22:21:52 +0900

    Initial commit

---
+++ src/main/java/com/dev/blog/BlogdevApplication.java
@@ -0,0 +1,12 @@
+package com.dev.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogdevApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dev/blog/HelloRestController.java
@@ -0,0 +1,12 @@
+package com.dev.blog;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+	@RequestMapping("/")
+	public String index() {
+		return "hello world!";
+	}
+}
